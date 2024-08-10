d88ce125bade5bb3e038a7661dda4dd6b8166143
Saket Jain <saketj21@iitk.ac.in>
2024-08-10 22:17:22 +0530

    game loop

--- homePage/homePage.js
+++ homePage/homePage.js
@@ -1,9 +1,13 @@
 
 const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 300;
 
 let scaleRatio=1;
+let previousTime = null;
+let notStarted = true;
+let gameOver = false;
 
 
 function setScreenSize() {
@@ -42,3 +46,30 @@ function showGameOver() {
   const y = canvas.height / 2;
   ctx.fillText("GAME OVER", x, y);
 }
+
+function gameLoop(currentTime){
+  if (previousTime === null) {
+    previousTime = currentTime;
+    requestAnimationFrame(gameLoop);
+    return;
+  }
+  const frameTimeDelta = currentTime - previousTime;
+  previousTime = currentTime;
+  
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  console.log("game loop");
+
+  if(notStarted){
+    showStartGame();
+  }
+
+  if(gameOver){
+    showGameOver();
+  }
+
+  requestAnimationFrame(gameLoop);
+
+}
+
+requestAnimationFrame(gameLoop);
