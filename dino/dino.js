ef954046935102d71e2b4edfa703a1c37a4efcfc
Rishi Poonia <rishi34punia@gmail.com>
2024-08-12 02:58:49 +0530

    Added Dino

---
+++ dino/dino.js
@@ -0,0 +1,16 @@
+export default class Dino{
+    constructor(ctx, width, height, scaleRatio){
+        this.ctx = ctx;
+        this.canvas = ctx.canvas;
+        this.width = width / scaleRatio;
+        this.height = height / scaleRatio;
+        this.x = 20;
+        this.y = this.canvas.height - height + 75;
+        this.image = new Image();
+        this.image.src = "../dino/dino_run_2.png";
+    }
+
+    draw(){
+        this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+}
