16d436e703968900795c21d39e89b9a81903bb5b
saketj21 <118554252+saketj21@users.noreply.github.com>
2024-09-09 15:57:27 +0530

    code complexity matrix add

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,24 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import complexityPlugin from "eslint-plugin-complexity";
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: globals.browser,
+    },
+    plugins: {
+      complexity: complexityPlugin,  // Assign the plugin object directly
+    },
+    rules: {
+      'complexity': ['error', { 'max': 10 }],
+      'max-depth': ['error', 4],
+      'max-lines-per-function': ['error', 50],
+      'max-params': ['error', 4],
+      'max-statements': ['error', 15],
+    },
+  },
+  pluginJs.configs.recommended
 ];
